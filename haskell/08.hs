7bfba0c6a24bb06dfe066d1d7d359ab238b5d883
Ben Kolera <Ben.Kolera@gmail.com>
2010-06-28 08:56:07 +1000

    Adding typing note to haskell q08

--- haskell/08.hs
+++ haskell/08.hs
@@ -1,3 +1,9 @@
+-- Careful now. If you specify a type here you must define an equality operator
+-- for your type a. If you leave it off, then the compiler is a ninja and infer
+-- the type of the list from how you use it. So long as it is a type that has ==
+-- defined, you shall have no problems. 
+--
+--compress              :: [a] -> [a] 
 compress []           = []
 compress [a]          = [a]
 compress (a : b : xs)
